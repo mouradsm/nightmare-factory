5bbc2356ad9b68d7f7fdd3e71db5f98ef9736221
Diego Moura <diegosouza20049@hotmail.com>
2017-04-19 14:36:41 -0300

    close Milistone #1 - Implemented basic select

--- src/main/java/br/mil/fab/Constants.java
+++ src/main/java/br/mil/fab/Constants.java
@@ -5,8 +5,7 @@ public final class Constants {
     private Constants() {}
 
     /** API Route **/
-    public static final String API_GET = "/select/:table_name/:id";
-    public static final String API_LIST_ALL = "/select/:table_name";
+    public static final String API_GET = "/select/:table_name";
     public static final String API_CREATE = "/insert/:table_name";
     public static final String API_UPDATE = "/update/:table_name/:id";
     public static final String API_DELETE = "/delete/:table_name/:id";
--- src/main/java/br/mil/fab/NightmareVerticle.java
+++ src/main/java/br/mil/fab/NightmareVerticle.java
@@ -3,8 +3,16 @@ package br.mil.fab;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.CorsHandler;
 
@@ -18,9 +26,12 @@ public class NightmareVerticle extends AbstractVerticle {
     private static final String HTTP_HOST = "0.0.0.0";
     private static final int HTTP_PORT = 8080;
 
+    private Logger logger = LoggerFactory.getLogger("br.mil.fab.NightmareVerticle");
+    private JDBCClient jdbcClient;
+
     @Override
     public void start(Future<Void> fut) {
-
+        initData();
 
 
         Router router = Router.router(vertx);
@@ -44,14 +55,21 @@ public class NightmareVerticle extends AbstractVerticle {
 
         router.route().handler(BodyHandler.create());
 
+        //routes
+        router.get(Constants.API_GET).handler(this::handlerGetById);
+        //router.get(Constants.API_LIST_ALL).handler(this::handlerGetAll);
+//        router.get(Constants.API_CREATE).handler(this::handlerCreate);
+//        router.get(Constants.API_UPDATE).handler(this::handlerUpdate);
+//        router.get(Constants.API_DELETE).handler(this::handlerDeleteOne);
+//        router.get(Constants.API_DELETE_ALL).handler(this::handlerDeleteAll);
 
-        router.get("/api/select/:table_name").handler(rc -> {
-
-            String table_name = rc.request().getParam("table_name");
-
-            rc.response().putHeader("content-type", "application/json")
-                    .end(new JsonObject().put("table_name", table_name).encode());
-        });
+//        router.get("/api/select/:table_name").handler(rc -> {
+//
+//            String table_name = rc.request().getParam("table_name");
+//
+//            rc.response().putHeader("content-type", "application/json")
+//                    .end(new JsonObject().put("table_name", table_name).encode());
+//        });
 
         vertx.createHttpServer().requestHandler(router::accept).listen(
                 HTTP_PORT, HTTP_HOST, result -> {
@@ -61,5 +79,63 @@ public class NightmareVerticle extends AbstractVerticle {
                         fut.fail(result.cause());
                     }
                 });
+    }
+
+    private void handlerGetById(RoutingContext context) {
+
+        String table_name   = context.request().getParam("table_name");
+        String columnNames  = context.request().getParam("columnNames") == null ? "*"
+                : context.request().getParam("columnNames");
+
+        String where = context.request().getParam("where");
+
+        if(table_name == null) {
+            sendError(400, context.response());
+            logger.info("table name can't be Null" );
+        }
+
+        jdbcClient.getConnection(conn -> {
+            if(conn.succeeded()) {
+                SQLConnection connection = conn.result();
+
+                StringBuilder sql = new StringBuilder();
+                sql.append("SELECT ");
+                sql.append(columnNames);
+                sql.append(" FROM ");
+                sql.append(table_name);
+                sql.append(" WHERE ");
+                sql.append(where);
+
+                logger.info(sql);
+
+                connection.query(sql.toString(), rs -> {
+
+                    if(rs.succeeded()) {
+                        ResultSet result = rs.result();
+                        context.response()
+                                .putHeader("content-type", "application/json")
+                                .end(Json.encodePrettily(result));
+                    }else {
+                        sendError(400, context.response());
+                    }
+                });
+            }
+        });
+    }
+
+    private void sendError(int statusCode, HttpServerResponse response){
+        response.setStatusCode(statusCode).end();
+    }
+
+    private void initData() {
+
+        JsonObject config = new JsonObject()
+                .put("url", "jdbc:oracle:thin:@10.52.132.91:1521:homolog")
+                .put("driver_class", "oracle.jdbc.driver.OracleDriver")
+                .put("user", "consulta")
+                .put("password", "53con90")
+                .put("max_pool_size", 30);
+
+        this.jdbcClient = JDBCClient.createShared(vertx, config);
     }
 }
